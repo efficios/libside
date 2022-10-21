dda82f4687fc3a8c381b6640f4987649a6101ae8
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2022-10-21 14:53:57 -0400

    update comment
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- src/side.c
+++ src/side.c
@@ -15,7 +15,7 @@ void side_call(const struct side_event_description *desc, const struct side_arg_
 		abort();
 	}
 	if (side_unlikely(*desc->enabled & SIDE_EVENT_ENABLED_KERNEL_USER_EVENT_MASK)) {
-		// TODO: call kernel ioctl.
+		// TODO: call kernel write.
 	}
 	//TODO: replace tracer_call by rcu iteration on list of registered callbacks
 	tracer_call(desc, sav_desc, NULL);
@@ -26,7 +26,7 @@ void side_call_variadic(const struct side_event_description *desc,
 	const struct side_arg_dynamic_event_struct *var_struct)
 {
 	if (side_unlikely(*desc->enabled & SIDE_EVENT_ENABLED_KERNEL_USER_EVENT_MASK)) {
-		// TODO: call kernel ioctl.
+		// TODO: call kernel write.
 	}
 	//TODO: replace tracer_call by rcu iteration on list of registered callbacks
 	tracer_call_variadic(desc, sav_desc, var_struct, NULL);
