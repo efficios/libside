538bc2f212b1131500d022415cc95fcc16cccfc3
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2024-02-19 13:58:55 -0500

    Implement side_cpu_relax for x86
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- src/compiler.h
+++ src/compiler.h
@@ -6,11 +6,19 @@
 #ifndef _SIDE_COMPILER_H
 #define _SIDE_COMPILER_H
 
+#if defined(__x86_64__) || defined(__i386__)
+static inline
+void side_cpu_relax(void)
+{
+	asm volatile ("rep; nop" : : : "memory");
+}
+#else
 //TODO: implement per-architecture busy loop "pause"/"rep; nop;" instruction.
 static inline
 void side_cpu_relax(void)
 {
 	asm volatile ("" : : : "memory");
 }
+#endif
 
 #endif /* _SIDE_COMPILER_H */
